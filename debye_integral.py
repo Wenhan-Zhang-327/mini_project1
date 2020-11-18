78df90ee988800b4c9eed8bb78218012aee18270
Wenhan-Zhang-327 <72666776+Wenhan-Zhang-327@users.noreply.github.com>
2020-11-18 00:38:56 +0000

    Update debye_integral.py

--- debye_integral.py
+++ debye_integral.py
@@ -1,41 +1,55 @@
 import numpy
-from scipy.integrate import quad
+from scipy.integrate import nquad
 from scipy.special import jn
 from numpy import exp,pi,sin,cos,sqrt,arctan2
-def debyr_integral_vector(x,y,z,alpha,lam,f,ex,ey):
+def debyr_integral_vector(point,alpha,lam,f,E_x,E_y):
     
-
-    
-    def debyr_integral(x,y,z,alpha,lam,f,ex,ey): 
+    def debyr_integral(x,y,z,alpha,lam,f,E_x,E_y): 
         
         kr = 2*pi/lam*sqrt(x*x+y*y)
         kz = 2*pi/lam*z
         phi = arctan2(y,x)
         
-        def complex_quadrature(func, a, b, **kwargs):
-            def real_func(x):
-                return numpy.real(func(x))
-            def imag_func(y):
-                return numpy.imag(func(y))
-            real_integral = quad(real_func, a, b, **kwargs)
-            imag_integral = quad(imag_func, a, b, **kwargs)
+        def complex_dblquadrature(func, a, b, c, d, **kwargs):
+            
+            def real_func(x,y):
+                return numpy.real(func(x,y))
+            def imag_func(x,y):
+                return numpy.imag(func(x,y))
+            def bounds_x(y):
+                return [a, b]
+            def bounds_y():
+                return [c, d]
+            
+            real_integral = nquad(real_func, [bounds_x, bounds_y], **kwargs)
+            imag_integral = nquad(imag_func, [bounds_x, bounds_y], **kwargs)
+            
             return real_integral[0] + 1j*imag_integral[0]
-        def i0(θ):
-            return sqrt(cos(θ))*sin(θ)*(cos(θ)+1)*jn(0,kr*sin(θ))*exp(1j*kz*cos(θ))
-        def i1(θ):
-            return sqrt(cos(θ))*sin(θ)*sin(θ)*jn(1,kr*sin(θ))*exp(1j*kz*cos(θ))
-        def i2(θ):
-            return sqrt(cos(θ))*sin(θ)*(cos(θ)-1)*jn(2,kr*sin(θ))*exp(1j*kz*cos(θ))
-        I0 = complex_quadrature(lambda θ:i0(θ), 0, alpha)
-        I1 = complex_quadrature(lambda θ:i1(θ), 0, alpha)
-        I2 = complex_quadrature(lambda θ:i2(θ), 0, alpha)
-    
-        Ex = ex*(I0+I2*cos(2*phi))+ey*I2*sin(2*phi)*1j*2*pi/lam*f
-        Ey = ey*(I0-I2*cos(2*phi))+ex*I2*sin(2*phi)*1j*2*pi/lam*f
-        Ez = 2*I1*ex*cos(phi)+ey*sin(phi)*2*pi/lam*f
+        
+        def e(theta,psi): 
+            q1 = cos(theta) + 1
+            q2 = cos(theta) - 1
+            q3 = 2*sin(theta)
+            q4 = 2*cos(theta)
+            
+            a_theta = 0.5*sqrt(cos(theta))
+            
+            ex = a_theta*(E_x*(q1+q2*cos(2*psi))+E_y*q2*sin(2*psi))*1j/lam*f*exp(1j*(kz*cos(theta)+kr*sin(theta)*cos(psi-phi)))
+            ey = a_theta*(E_y*(q1-q2*cos(2*psi))+E_x*q2*sin(2*psi))*1j/lam*f*exp(1j*(kz*cos(theta)+kr*sin(theta)*cos(psi-phi)))
+            ez = a_theta*(-q3)*(E_x*cos(psi)+E_y*sin(psi))*1j/lam*f*exp(1j*(kz*cos(theta)+kr*sin(theta)*cos(psi-phi)))
+            
+            return (ex,ey,ez)
+        
+        Ex = complex_dblquadrature(lambda theta, psi: e(theta,psi)[0], 0, alpha, 0, 2*pi)
+        Ey = complex_dblquadrature(lambda theta, psi: e(theta,psi)[1], 0, alpha, 0, 2*pi)
+        Ez = complex_dblquadrature(lambda theta, psi: e(theta,psi)[2], 0, alpha, 0, 2*pi)
         
         return (Ex,Ey,Ez)
+    
     debyr_integral_v = numpy.vectorize(debyr_integral)
-    return numpy.transpose(debyr_integral_v(x,y,z,alpha,lam,f,ex,ey))
+    p = numpy.transpose(point)
+    
+    return numpy.transpose(debyr_integral_v(p[0],p[1],p[2],alpha,lam,f,E_x,E_y))
 
-debyr_integral_vector([1,2,3,4],[2,2,2,2],[3,3,3,3],0.5,4,2,1,0)
+points = [[1,2,3],[2,2,3],[3,2,3],[4,2,3]]
+debyr_integral_vector(points,0.5,4,2,1,0)
