2a78c8b4d8cf13dd1861e8ddabbf6f944ecf8adc
Wenhan-Zhang-327 <72666776+Wenhan-Zhang-327@users.noreply.github.com>
2020-11-22 05:29:55 +0000

    Update debye_integral_mid_plot.py

--- debye_integral_mid_plot.py
+++ debye_integral_mid_plot.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 # from scipy.integrate import nquad
 # from scipy.special import jn
 from numpy import exp,pi,sin,cos,sqrt,arctan2     
-def getPhaseMask(w,z1,z2,z3,z4):
+def getPhaseMask(theta,psi,f,z1,z2,z3,z4):
     """
     Calculates the phase mask to reproduce the 4 main aberations for high NA objectives
     ----------
@@ -19,23 +19,17 @@ def getPhaseMask(w,z1,z2,z3,z4):
         W x W square array containing the phase mask
     
     """
-    lims = np.linspace(-1,1/2,w)
-    x, y = np.meshgrid(lims,lims)
-    rho = np.sqrt(x**2 + y**2) 
-    phi = np.arctan2(y, x)
-    
-    mask = np.zeros([w,w])
-    
-    mask += z1* np.sqrt(3)*(2*(rho**2) - 1)
-    mask += z2* np.sqrt(6)*(rho**2)*np.cos(2*phi)
-    mask += z3* np.sqrt(6)*(rho**2)*np.sin(2*phi)
-    mask += z4* np.sqrt(5)*(6*(rho**4) - 6*(rho**2) + 1)
+    mask = 0
+    mask += z1* np.sqrt(3)*(2*((f*np.sin(theta))**2) - 1)
+    mask += z2* np.sqrt(6)*((f*np.sin(theta))**2)*np.cos(2*psi)
+    mask += z3* np.sqrt(6)*((f*np.sin(theta))**2)*np.sin(2*psi)
+    mask += z4* np.sqrt(5)*(6*((f*np.sin(theta))**4) - 6*((f*np.sin(theta))**2) + 1)
    
     mask = mask % (2 * np.pi) -np.pi
         
     return mask
 
-def addPhaseFeature(mask,feature,Z1,Z2):
+def addPhaseFeature(mask,theta,psi,f,feature,Z1,Z2):
     """
     Calculates the phase mask to reproduce typical PSF modification
     ----------
@@ -50,33 +44,29 @@ def addPhaseFeature(mask,feature,Z1,Z2):
         W x W square array containing the altered phase mask
     
     """
-    w = mask.shape[0]
-    lims = np.linspace(-1/2,1/2,w)
-    x, y = np.meshgrid(lims,lims)
-    rho = np.sqrt(x**2 + y**2) 
-    phi = np.arctan2(y, x)
+    
     if feature == 'STED':
 
-        mask = mask+phi*Z1
+        mask = mask+psi*Z1
   
     elif feature == 'bessel':
 
-        mask = mask+np.pi*2*np.cos(Z1*rho)
+        mask = mask+np.pi*2*np.cos(Z1*(f*np.sin(theta)))
 
     elif feature == 'bessel phase':
 
-        mask = mask+Z1*rho
+        mask = mask+Z1*(f*np.sin(theta))
 
     elif feature == 'bottle':
         
-        mask[rho<Z1] += np.pi/2
-        mask[rho<Z2] += 0
+        if Z2<(f*np.sin(theta))<Z1:
+            mask = mask + pi/2
 
     mask = mask % (2 * np.pi) -np.pi
         
     return mask
 
-def getPupil(w,beam,Z1,Z2):
+def getPupil(theta,psi,f,beam,Z1,Z2):
     """
     Calculates the beam amplitude at the back of the lens
     ----------
@@ -91,70 +81,77 @@ def getPupil(w,beam,Z1,Z2):
         W x W square array containing the altered phase mask
     
     """
-    lims = np.linspace(-1,1,w)
-    x, y = np.meshgrid(lims,lims)
-    rho = np.sqrt(x**2 + y**2) 
-    phi = np.arctan2(y, x)
 
-    pupil = np.zeros([w,w])
+    pupil = 0
 
     if beam == 'Gaussian':
 
-        pupil = np.exp(-rho**2/(2*Z1**2))
+        pupil = np.exp(-(f*np.sin(theta))**2/(2*Z1**2))
     
     elif beam == 'Airy':
 
-        pupil[rho<Z1] = 1
-        pupil[rho<Z2] = 0
+        if Z2<(f*np.sin(theta))<Z1:
+            pupil = 1
+        else:
+            pupil = 0
+                
 
-    elif beam == 'SIM':
+    # elif beam == 'SIM':
 
-        pupil[rho<Z1] = 1
-        pupil = np.roll(pupil,(int(Z2*w))) + np.roll(pupil,(-int(Z2*w)))
+    #     pupil[rho<Z1] = 1
+    #     pupil = np.roll(pupil,(int(Z2*w))) + np.roll(pupil,(-int(Z2*w)))
 
-    elif beam =='SPIM':
+    # elif beam =='SPIM':
 
-        pupil[rho<Z1] = 1
-        pupil = np.roll(pupil,(int(Z2*w)))
+    #     pupil[rho<Z1] = 1
+    #     pupil = np.roll(pupil,(int(Z2*w)))
 
     else:
-        pupil[rho<Z1] = 1
+        pupil = 1
 
-    pupil = pupil/np.amax(pupil)
+    # pupil = pupil/np.amax(pupil)
 
     return pupil
 
-def shiftSpot(pupil, beamCentre):
+# def shiftSpot(pupil, beamCentre):
 
-    w = pupil.shape[0]
-    padLength = int(np.round(w/2))
-    lims = np.linspace(-1,1,w)
+#     w = pupil.shape[0]
+#     padLength = int(np.round(w/2))
+#     lims = np.linspace(-1,1,w)
+#     x, y = np.meshgrid(lims,lims)
+#     rho = np.sqrt(x**2 + y**2) 
+#     pupil[rho>1]=0
+#     pupil = pupil[::2,::2]
+#     newPupil = np.pad(pupil, ((padLength, padLength), (padLength, padLength)), 'constant')
+#     #newPupil = np.roll(newPupil,int(beamCentre*padLength))
+
+
+#     return newPupil
+
+# def addLinearPhase(mask,tilt):
+
+#     w = mask.shape[0]
+#     lims = np.linspace(-1,1,w)
+#     x, y = np.meshgrid(lims,lims)
+#     linPhase = x*tilt
+#     linPhase = linPhase % (2 * np.pi) -np.pi
+#     mask = mask + linPhase
+#     mask = mask % (2 * np.pi) -np.pi
+
+#     return mask
+
+def beam_sphere(theta,psi,f):
+    mask = getPhaseMask(theta,psi,f,0,0,0,0)
+    mask = addPhaseFeature(mask,theta,psi,f,'STED',1,0)
+    pupil = getPupil(theta,psi,f,'Gaussian',0.8,0.4)
+    beam = pupil*np.exp(1j*mask)
+    return beam
+   
+def debyr_integral(alpha,lam,f,E_x,E_y): 
+    w = 512
+    lims = np.linspace(-10,10,w)
     x, y = np.meshgrid(lims,lims)
-    rho = np.sqrt(x**2 + y**2) 
-    pupil[rho>1]=0
-    pupil = pupil[::2,::2]
-    newPupil = np.pad(pupil, ((padLength, padLength), (padLength, padLength)), 'constant')
-    #newPupil = np.roll(newPupil,int(beamCentre*padLength))
-
-
-    return newPupil
-
-def addLinearPhase(mask,tilt):
-
-    w = mask.shape[0]
-    lims = np.linspace(-1,1,w)
-    x, y = np.meshgrid(lims,lims)
-    linPhase = x*tilt
-    linPhase = linPhase % (2 * np.pi) -np.pi
-    mask = mask + linPhase
-    mask = mask % (2 * np.pi) -np.pi
-
-    return mask   
-def debyr_integral(beam,alpha,lam,f,E_x,E_y): 
-    w = beam.shape[0]
-    lims = np.linspace(-1,1,w)
-    x, y = np.meshgrid(lims,lims)
-    z = f
+    z = 0
     k = 2*pi/lam
     r = sqrt(x**2+y**2)
     phi = arctan2(y,x)
@@ -172,6 +169,7 @@ def debyr_integral(beam,alpha,lam,f,E_x,E_y):
                 I += hx*hy*f(xi, yj)
         return I
         
+        
     def e(theta,psi): 
         q1 = cos(theta) + 1
         q2 = cos(theta) - 1
@@ -180,36 +178,43 @@ def debyr_integral(beam,alpha,lam,f,E_x,E_y):
             
         a_theta = 0.5*sqrt(cos(theta))
             
-        ex = a_theta*(E_x*(q1+q2*cos(2*psi))+E_y*q2*sin(2*psi))*(-1j)/lam*f*exp(1j*(k*z*cos(theta)+k*r*sin(theta)*cos(psi-phi)))*beam
-        ey = a_theta*(E_y*(q1-q2*cos(2*psi))+E_x*q2*sin(2*psi))*(-1j)/lam*f*exp(1j*(k*z*cos(theta)+k*r*sin(theta)*cos(psi-phi)))*beam
-        ez = a_theta*(-q3)*(E_x*cos(psi)+E_y*sin(psi))*(-1j)/lam*f*exp(1j*(k*z*cos(theta)+k*r*sin(theta)*cos(psi-phi)))*beam
+        ex = a_theta*(E_x*(q1+q2*cos(2*psi))+E_y*q2*sin(2*psi))*(-1j)/lam*f*exp(1j*(k*z*cos(theta)+k*r*sin(theta)*cos(psi-phi)))*sin(theta)*beam_sphere(theta,psi,f)
+        ey = a_theta*(E_y*(q1-q2*cos(2*psi))+E_x*q2*sin(2*psi))*(-1j)/lam*f*exp(1j*(k*z*cos(theta)+k*r*sin(theta)*cos(psi-phi)))*sin(theta)*beam_sphere(theta,psi,f)
+        ez = a_theta*(-q3)*(E_x*cos(psi)+E_y*sin(psi))*(-1j)/lam*f*exp(1j*(k*z*cos(theta)+k*r*sin(theta)*cos(psi-phi)))*sin(theta)*beam_sphere(theta,psi,f)
         return (ex,ey,ez)
+    #def beam_sphere(theta,psi):
         
-    Ex = np.abs(midpoint_double1(lambda theta, psi: e(theta,psi)[0], 0, alpha, 0, 2*pi, 5, 5))
-    Ey = np.abs(midpoint_double1(lambda theta, psi: e(theta,psi)[1], 0, alpha, 0, 2*pi, 5, 5))
-    Ez = np.abs(midpoint_double1(lambda theta, psi: e(theta,psi)[2], 0, alpha, 0, 2*pi, 5, 5))
+        
+        
+    Ex = np.abs(midpoint_double1(lambda theta, psi: e(theta,psi)[0], 0, alpha, 0, 2*pi, 20, 20))
+    Ey = np.abs(midpoint_double1(lambda theta, psi: e(theta,psi)[1], 0, alpha, 0, 2*pi, 20, 20))
+    Ez = np.abs(midpoint_double1(lambda theta, psi: e(theta,psi)[2], 0, alpha, 0, 2*pi, 20, 20))
     
     intensity += (Ex**2+Ey**2+Ez**2)
     return intensity
 
-mask = getPhaseMask(512,0,0,0,0)
-mask = addPhaseFeature(mask,'STED',1,0)
+# mask = getPhaseMask(512,1,0,0,0,0)[0]
+# theta1 = getPhaseMask(512,1,0,0,0,0)[1]
+# mask = addPhaseFeature(mask,1,'STED',1,0)[0]
+# theta2 = addPhaseFeature(mask,1,'STED',1,0)[1]
+# pupil = getPupil(512,'Airy',1,0.8,0.4)[0]
+# theta3 = getPupil(512,'Airy',1,0.8,0.4)[1]
 
-pupil = getPupil(512,'Airy',0.8,0.4)
-
-beam = pupil*np.exp(1j*mask)
-beam = np.pad(beam, (512, 512), 'constant', constant_values=0)
-sample = debyr_integral(beam,0.45*pi,0.68,2e8,1,0)
+# beam = pupil*np.exp(1j*mask)
+#beam = np.pad(beam, (512, 512), 'constant', constant_values=0)
+sample = debyr_integral(1,0.68,5,1,1)
+#sample = np.pad(sample, (512, 512), 'constant', constant_values=0)
 plt.figure(figsize=(10, 10))
 
 
 centre = np.shape(sample)[1]/2
-lim_up = int(centre+500)
-lim_down = int(centre-500)
+lim_up = int(centre+200)
+lim_down = int(centre-200)
 plt.subplot(1,3,1)
 plt.imshow(sample[lim_down:lim_up,lim_down:lim_up],cmap = 'gray')
-plt.subplot(1,3,2)
-plt.imshow(pupil,cmap = 'gray')
-plt.subplot(1,3,3)
-plt.imshow(mask,cmap = 'gray')
-plt.show()
+# plt.subplot(1,3,2)
+# plt.imshow(pupil,cmap = 'gray')
+# plt.subplot(1,3,3)
+# plt.imshow(mask,cmap = 'gray')
+# plt.show()
+
