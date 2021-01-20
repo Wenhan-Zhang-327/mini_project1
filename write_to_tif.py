3eb1c919ec521775e7631dc5b9ebecadf48b6ce4
Wenhan-Zhang-327 <72666776+Wenhan-Zhang-327@users.noreply.github.com>
2021-01-20 08:35:16 +0000

    Update write_to_tif.py

--- write_to_tif.py
+++ write_to_tif.py
@@ -2,15 +2,24 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numpy import exp,pi,sin,cos,sqrt,arctan2,tan   
 import tifffile as tiff
-ele = np.array([[[1,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]])
-ele2 = np.tile(ele, (40,40,40)) 
+w = 40
+a = 10
+b = 512-a*w
+ele1 =  np.zeros((w, w, w))
+ele1[0,0,0] = 1
+ele1[w-1,w-1,w-1] = 1
+ele2 = np.tile(ele1, (a,a,a)) 
+ele3 = np.pad(ele2, ((0,b),(int(b/2),int(b/2)),(int(b/2),int(b/2))), 'constant', constant_values=0) 
+plt.imshow(ele3[0,:,:],cmap = 'gray')
+tiff.imwrite('sample_sep.tif',ele3,photometric='minisblack')
+
 out_put = np.load('out_put.npy') 
 PSF = tiff.imread('PSF BW.tif')
+OTF = np.fft.fftshift(np.fft.fftn(PSF))
 image_sams = np.zeros((512,512,512))
 for i in range(353):
-    ele3 = np.pad(ele2, ((i,352-i),(176,176),(176,176)), 'constant', constant_values=0) 
-    out_put_sample = np.multiply(out_put,ele3)
-    OTF = np.fft.fftshift(np.fft.fftn(PSF))
+    sample = np.roll(ele3,i,axis=0)
+    out_put_sample = np.multiply(out_put,sample)
     sampleF = np.fft.fftshift(np.fft.fftn(out_put_sample))
     imageF = sampleF * OTF
     image = np.fft.fftshift(np.fft.ifftn(imageF))
