7dd5fcf54a7a59dbf540a92b91aa8153baa500d0
Wenhan-Zhang-327 <72666776+Wenhan-Zhang-327@users.noreply.github.com>
2021-01-25 22:24:21 +0000

    Update write_to_tif.py

--- write_to_tif.py
+++ write_to_tif.py
@@ -2,6 +2,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numpy import exp,pi,sin,cos,sqrt,arctan2,tan   
 import tifffile as tiff
+from skimage import io
+
+def saveResult(array,title):
+    array = array-np.amin(array)
+    array = 65535*array/np.amax(array)
+    svPath = title + '.tif'
+    array = array.astype(np.uint16)
+    io.imsave(svPath,array)
+    return []
+
+print('Generating sample...')
 w = 40
 a = 10
 b = 512-a*w
@@ -10,18 +21,53 @@ ele1[0,0,0] = 1
 ele1[w-1,w-1,w-1] = 1
 ele2 = np.tile(ele1, (a,a,a)) 
 ele3 = np.pad(ele2, ((0,b),(int(b/2),int(b/2)),(int(b/2),int(b/2))), 'constant', constant_values=0) 
-plt.imshow(ele3[0,:,:],cmap = 'gray')
+
+
 tiff.imwrite('sample_sep.tif',ele3,photometric='minisblack')
 
-out_put = np.load('out_put.npy') 
+
+
+print('Loading detection PSF...')
 PSF = tiff.imread('PSF BW.tif')
-OTF = np.fft.fftshift(np.fft.fftn(PSF))
-image_sams = np.zeros((512,512,512))
-for i in range(b):
-    sample = np.roll(ele3,i,axis=0)
-    out_put_sample = np.multiply(out_put,sample)
-    sampleF = np.fft.fftshift(np.fft.fftn(out_put_sample))
-    imageF = sampleF * OTF
-    image = np.fft.fftshift(np.fft.ifftn(imageF))
-    image_sams[i,:,:] = np.sum(np.abs(image),axis=0)
-tiff.imwrite('image_move.tif',image_sams,photometric='minisblack')
+saveResult(PSF,'detection PSF')
+
+print('Calculating detection OTF...')
+OTF = (np.fft.fftn(PSF))
+del PSF 
+saveResult(np.abs(OTF),'detection OTF')
+
+print('Loading excitation PSF...')
+out_put = np.float32(np.load('out_put.npy')) 
+saveResult(out_put,'excitation PSF')
+
+print('Generating lightsheet...')
+sheet = out_put[256,:,:]
+del out_put
+sheet = np.tile(sheet,(512, 1, 1))
+sheet = np.moveaxis(sheet,0,1)
+saveResult(sheet,'sheet')
+
+print('Rolling sample')
+temp = np.float32(np.zeros((512,512)))
+sample = np.roll(ele3,20,axis=0)
+
+print('Calculating fluoresence response')
+temp = np.multiply(sheet,sample)
+# temp = np.copy(sample) 
+saveResult(temp,'response')
+
+print('Calculating FT of fluoresence response')
+temp = (np.fft.fftn(temp))
+
+print('Clipping frequencies')
+temp = np.multiply(temp,OTF)
+saveResult(np.abs(temp),'F_spectrum')
+
+print('Moving to image space')
+temp = np.fft.fftshift(np.fft.ifftn(temp))
+
+print('Calculating detected signal')
+temp = np.sum(np.abs(temp),axis=0)
+
+print('Saving results')
+saveResult(temp,'result')
